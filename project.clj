354b0770e49f64bbcc4579e0b822ca262ab79fad
Oliver Hine <git@oliy.co.uk>
2017-06-08 19:18:56 +0100

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-learn "0.1.1-SNAPSHOT"
+(defproject re-learn "0.1.1"
   :description "Data-driven tutorials for reagent UIs"
   :url "https://github.com/oliyh/re-learn"
   :license {:name "Eclipse Public License"
