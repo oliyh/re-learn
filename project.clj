f079441311b75efb65450ba37b6e3651ca29f087
Oliver Hine <git@oliy.co.uk>
2017-06-08 18:57:36 +0100

    not signing github tags

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
-                  ["vcs" "tag"]
+                  ["vcs" "tag" "--no-sign"]
                   ["deploy" "clojars"]
                   ["change" "version" "leiningen.release/bump-version"]
                   ["vcs" "commit"]
